44f9f2a011c5d33ba12848fe0339f33b475a7363
KristiSeraj <serajkristi@gmail.com>
2022-12-15 10:11:42 -0800

    first commit

---
+++ markdown2html.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import os
+    from sys import argv
+
+    if len(argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
+    if not os.path.exists(argv[1]):
+        print(f"Missing {argv[1]}")
+        exit(1)
+    with open(argv[1], "r") as f:
+        with open(argv[2], "a") as nw:
+            for line in f:
+                count_tag = line.count("#")
+                new_l = line.strip("# ")
+                a = new_l.rstrip('\n')
+                nw.write(f"<h{count_tag}>{a}</h{count_tag}> \n")
