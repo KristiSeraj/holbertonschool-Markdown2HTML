81b163c041db9e6f5dc375962e656bb0347ecbf9
KristiSeraj <serajkristi@gmail.com>
2022-12-19 05:44:23 -0800

    checking

--- markdown2html.py
+++ markdown2html.py
@@ -14,16 +14,20 @@ if __name__ == "__main__":
     with open(argv[1], "r") as f:
         with open(argv[2], "a") as nw:
             line_count = 0
+            ord_line_c = 0
             for line in f:
                 if line.startswith("#") is True:
                     count_tag = line.count("#")
                     if count_tag > 0 and count_tag <= 6:
                         if line_count > 0:
                             nw.write("</ul>\n")
+                        if ord_line_c > 0:
+                            nw.write("</ol>\n")
                         new_l = line.strip("# ")
                         a = new_l.rstrip('\n')
                         nw.write(f"<h{count_tag}>{a}</h{count_tag}> \n")
                         line_count = 0
+                        ord_line_c = 0
                 if line.startswith("-") is True:
                     if line_count == 0:
                         nw.write("<ul>\n")
@@ -31,7 +35,17 @@ if __name__ == "__main__":
                     n_list_el = list_el.rstrip('\n')
                     nw.write(f"<li>{n_list_el}</li>\n")
                     line_count += 1
+                if line.startswith("*") is True:
+                    if ord_line_c == 0:
+                        nw.write("<ol>\n")
+                    ord_li = line.strip("* ")
+                    n_ord_li = ord_li.rstrip('\n')
+                    nw.write(f"<li>{n_ord_li}</li>\n")
+                    ord_line_c += 1
+            if ord_line_c > 0:
+                nw.write("</ol>\n")
             if line_count > 0:
                 nw.write("</ul>\n")
+
     print(end="")
     exit(0)
